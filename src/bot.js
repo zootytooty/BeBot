3c9c3488e052fca1ca7792ac6693943d054f52e6
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2021-06-27 17:19:10 +1000

    Remove FB dev setting

--- src/bot.js
+++ src/bot.js
@@ -45,9 +45,6 @@ const storage = null;
 // }
 
 const adapter = new FacebookAdapter({
-  // REMOVE THIS OPTION AFTER YOU HAVE CONFIGURED YOUR APP!
-  enable_incomplete: true,
-
   verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
   access_token: process.env.FACEBOOK_ACCESS_TOKEN,
   app_secret: process.env.FACEBOOK_APP_SECRET,
