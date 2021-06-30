509df63f6bbc325006c706aeecff3f152d0da9fa
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2021-06-30 23:26:26 +1000

    Update subtitle formatting

--- src/utils/get_gigs.js
+++ src/utils/get_gigs.js
@@ -3,11 +3,30 @@ const axios = require('axios');
 const GIGS_API =
   'https://v3dl6mmgz1.execute-api.ap-southeast-2.amazonaws.com/dev/gigs';
 
+function titleCase(str) {
+  const strLowerCase = str.toLowerCase().replace('_', ' ');
+  const wordArr = strLowerCase
+    .split(' ')
+    .map(
+      (currentValue) =>
+        currentValue[0].toUpperCase() + currentValue.substring(1)
+    );
+
+  return wordArr.join(' ');
+}
+
+function createSubtitle(gig) {
+  const line1 = `${titleCase(gig.venue)}, $${gig.price}`;
+  const line2 = `Doors: ${gig.doors_open}, Start: ${gig.music_starts}`;
+
+  return `${line1}\n${line2}`;
+}
+
 function gigToTemplateElement(data) {
   return {
     title: data.title,
     image_url: data.image_url,
-    subtitle: data.venue,
+    subtitle: createSubtitle(data),
     buttons: [
       {
         type: 'web_url',
