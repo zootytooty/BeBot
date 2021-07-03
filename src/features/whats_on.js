943e00715ac50aad7670a3686aadc1cdf0c0f001
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2021-07-04 00:14:00 +1000

    Handle different response types

--- src/features/whats_on.js
+++ src/features/whats_on.js
@@ -8,9 +8,13 @@ module.exports = async (controller) => {
 
       console.log(response);
 
-      await bot.reply(message, {
-        attachment: response,
-      });
+      if (typeof response === 'object') {
+        await bot.reply(message, {
+          attachment: response,
+        });
+      } else {
+        await bot.reply(message, response);
+      }
     } catch (e) {
       console.log(e);
     }
