9efdc5a16bd5d6e3f0eef42856f7b8768a2649bb
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2021-07-26 23:42:58 +1000

    Hopefully fixes the mongo storage implementation

--- src/bot.js
+++ src/bot.js
@@ -37,70 +37,76 @@ const wit = require('./middleware/wit-ai')({
   accessToken: process.env.WIT_TOKEN,
 });
 
-// code for upgraded mongodb storage package
-let storage = null;
-// GET MONGO Storage
-
-// Create a MongoClient and connect it.
-const mongoClient = new MongoClient(process.env.MONGO_URI, {
-  useUnifiedTopology: true,
-});
-await mongoClient.connect();
-
-// Grab a collection handle off the connected client
-const collection = mongoClient.getCollection(mongoClient);
-
-// Create a MongoDbStorage, supplying the collection to it.
-storage = new MongoDbStorage(collection);
-
-const adapter = new FacebookAdapter({
-  verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
-  access_token: process.env.FACEBOOK_ACCESS_TOKEN,
-  app_secret: process.env.FACEBOOK_APP_SECRET,
-});
-
-// emit events based on the type of facebook event being received
-adapter.use(new FacebookEventTypeMiddleware());
-
-const controller = new Botkit({
-  webhook_uri: '/api/messages',
-  adapter,
-  storage,
-});
-
-if (process.env.CMS_URI) {
-  controller.usePlugin(
-    new BotkitCMSHelper({
-      uri: process.env.CMS_URI,
-      token: process.env.CMS_TOKEN,
-    })
-  );
+//
+async function initBotStorage() {
+  const mongoClient = new MongoClient(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+  });
+  await mongoClient.connect();
+  const collection = MongoDbStorage.getCollection(mongoClient);
+  return new MongoDbStorage(collection);
 }
 
-// use middleware for any hits to the receive endpoint
-controller.middleware.receive.use(wit.receive);
+(async () => {
+  try {
+    let storage = null;
 
-// Once the bot has booted up its internal services, you can use them to do stuff.
-controller.ready(() => {
-  // load traditional developer-created local custom feature modules
-  controller.loadModules(`${__dirname}/features`);
+    if (process.env.MONGO_URI) {
+      storage = await initBotStorage();
+    }
 
-  /* catch-all that uses the CMS to trigger dialogs */
-  if (controller.plugins.cms) {
-    controller.on('message,direct_message', async (bot, message) => {
-      let results = false;
-      results = await controller.plugins.cms.testTrigger(bot, message);
-
-      if (results !== false) {
-        // do not continue middleware!
-        return false;
-      }
-
-      return true;
+    const adapter = new FacebookAdapter({
+      verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
+      access_token: process.env.FACEBOOK_ACCESS_TOKEN,
+      app_secret: process.env.FACEBOOK_APP_SECRET,
     });
-  }
-});
 
-controller.webserver.get('/', (req, res) =>
-  res.send(`Beep bop boop, this is Zoot`)
-);
+    // emit events based on the type of facebook event being received
+    adapter.use(new FacebookEventTypeMiddleware());
+
+    const controller = new Botkit({
+      webhook_uri: '/api/messages',
+      adapter,
+      storage,
+    });
+
+    if (process.env.CMS_URI) {
+      controller.usePlugin(
+        new BotkitCMSHelper({
+          uri: process.env.CMS_URI,
+          token: process.env.CMS_TOKEN,
+        })
+      );
+    }
+
+    // use middleware for any hits to the receive endpoint
+    controller.middleware.receive.use(wit.receive);
+
+    // Once the bot has booted up its internal services, you can use them to do stuff.
+    controller.ready(() => {
+      // load traditional developer-created local custom feature modules
+      controller.loadModules(`${__dirname}/features`);
+
+      /* catch-all that uses the CMS to trigger dialogs */
+      if (controller.plugins.cms) {
+        controller.on('message,direct_message', async (bot, message) => {
+          let results = false;
+          results = await controller.plugins.cms.testTrigger(bot, message);
+
+          if (results !== false) {
+            // do not continue middleware!
+            return false;
+          }
+
+          return true;
+        });
+      }
+    });
+
+    controller.webserver.get('/', (req, res) =>
+      res.send(`Beep bop boop, this is Zoot`)
+    );
+  } catch (ex) {
+    console.error(ex);
+  }
+})();
