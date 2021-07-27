e2c93c4b38eba7dacd928415f8ce0dc043763604
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2021-07-27 23:38:36 +1000

    Change event list

--- src/features/help.js
+++ src/features/help.js
@@ -1,8 +1,8 @@
 module.exports = async (controller) => {
   controller.on(
-    'message,direct_message',
-    'message,facebook_postback',
+    'message,direct_message,facebook_postback',
     async (bot, message) => {
+      console.log(message);
       try {
         if (message.intents[0].name === 'help') {
           const response = `Be bop zoot, here to help :) If you've found a bug, would like to request a feature or want to see what I can do, please headover to my homepage:
