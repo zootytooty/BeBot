c31cda0ca93067bd095b9003699fb16d554a2b45
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2021-07-27 23:42:04 +1000

    Remove logging & add postback to what's on

--- src/features/help.js
+++ src/features/help.js
@@ -2,7 +2,6 @@ module.exports = async (controller) => {
   controller.on(
     'message,direct_message,facebook_postback',
     async (bot, message) => {
-      console.log(message);
       try {
         if (message.intents[0].name === 'help') {
           const response = `Be bop zoot, here to help :) If you've found a bug, would like to request a feature or want to see what I can do, please headover to my homepage:
--- src/features/whats_on.js
+++ src/features/whats_on.js
@@ -2,23 +2,26 @@ const queryBuilder = require('../utils/query_builder');
 const getGigs = require('../utils/get_gigs');
 
 module.exports = async (controller) => {
-  controller.on('message,direct_message', async (bot, message) => {
-    try {
-      if (message.intents[0].name === 'whats_on') {
-        const response = await getGigs(queryBuilder(message));
+  controller.on(
+    'message,direct_message,facebook_postback',
+    async (bot, message) => {
+      try {
+        if (message.intents[0].name === 'whats_on') {
+          const response = await getGigs(queryBuilder(message));
 
-        console.log(response);
+          console.log(response);
 
-        if (typeof response === 'object') {
-          await bot.reply(message, {
-            attachment: response,
-          });
-        } else {
-          await bot.reply(message, response);
+          if (typeof response === 'object') {
+            await bot.reply(message, {
+              attachment: response,
+            });
+          } else {
+            await bot.reply(message, response);
+          }
         }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
     }
-  });
+  );
 };
