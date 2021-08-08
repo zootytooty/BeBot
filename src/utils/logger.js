f765614489dbee2693f992195a58fa10dd790be1
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2021-08-08 22:47:11 +1000

    Another attempt

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,36 +1,43 @@
+/* eslint-disable no-underscore-dangle */
 const { MongoClient } = require('mongodb');
 
 const logConversation = async (conversation) => {
+  // Clean up the Wit attributes
+  if (Object.prototype.hasOwnProperty.call(conversation, 'entities')) {
+    delete conversation.entities;
+  }
+  if (Object.prototype.hasOwnProperty.call(conversation, 'traits')) {
+    delete conversation.traits;
+  }
+  if (Object.prototype.hasOwnProperty.call(conversation, 'intents')) {
+    delete conversation.intents;
+  }
+
+  // Facebook sends everything including keys. We don't want to store them
+  delete conversation.context._adapter.options.access_token;
+  delete conversation.context._adapter.options.verify_token;
+  delete conversation.context._adapter.options.app_secret;
+
   const client = new MongoClient(process.env.MONGO_URI, {
     useUnifiedTopology: true,
   });
 
-  client.connect((err, db) => {
-    if (err) throw err;
-    const dbo = db.db('zootdb');
+  try {
+    await client.connect();
 
-    dbo.collection('logs').insertOne(conversation, (error, res) => {
-      if (error) throw error;
-      console.log(res);
-      db.close();
-    });
-  });
+    return client;
+    //   const db = client.db('zootdb');
+    //   const logs = db.collection('logs');
 
-  // try {
-  //   await client.connect();
+    // //   const result = await logs.insertOne(conversation);
 
-  //   const db = client.db('zootdb');
-  //   const logs = db.collection('logs');
-
-  // //   const result = await logs.insertOne(conversation);
-
-  //   return result;
-  // } catch (ex) {
-  //   console.log(ex);
-  //   return ex;
-  // } finally {
-  //   await client.close();
-  // }
+    //   return result;
+  } catch (ex) {
+    console.log(ex);
+    return ex;
+  } finally {
+    await client.close();
+  }
   //   console.log(client, conversation);
 };
 
