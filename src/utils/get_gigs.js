17db9094667c14c71d73d2248fa52636739314a3
Trent Howard <trent.howard.42@gmail.com>
2021-08-13 16:35:36 +1000

    clean up a couple funcs

--- src/utils/get_gigs.js
+++ src/utils/get_gigs.js
@@ -4,38 +4,32 @@ const chunk = require('lodash/chunk');
 const GIGS_API =
   'https://v3dl6mmgz1.execute-api.ap-southeast-2.amazonaws.com/dev/gigs';
 
-function titleCase(str) {
-  const strLowerCase = str.toLowerCase().replace('_', ' ');
-  const wordArr = strLowerCase
-    .split(' ')
-    .map(
-      currentValue => currentValue[0].toUpperCase() + currentValue.substring(1)
-    );
+const titleCase = str =>
+  str
+    .toLowerCase()
+    .split('_')
+    .map(word => word[0].toUpperCase() + word.substring(1))
+    .join(' ');
 
-  return wordArr.join(' ');
-}
-
-function createSubtitle(gig) {
+const createSubtitle = gig => {
   const line1 = `${titleCase(gig.venue)}, $${gig.price}`;
   const line2 = `Doors: ${gig.doors_open}, Start: ${gig.music_starts}`;
 
   return `${line1}\n${line2}`;
-}
+};
 
-function gigToTemplateElement(data) {
-  return {
-    title: data.title,
-    image_url: data.image_url,
-    subtitle: createSubtitle(data),
-    buttons: [
-      {
-        type: 'web_url',
-        url: data.url,
-        title: 'View Show',
-      },
-    ],
-  };
-}
+const gigToElement = gig => ({
+  title: gig.title,
+  image_url: gig.image_url,
+  subtitle: createSubtitle(gig),
+  buttons: [
+    {
+      type: 'web_url',
+      url: gig.url,
+      title: 'View Show',
+    },
+  ],
+});
 
 const getGigs = async (query = {}) => {
   try {
@@ -50,7 +44,7 @@ const getGigs = async (query = {}) => {
     const chunkyGigs = chunk(data, 10);
 
     const messages = chunkyGigs.map(gigs => {
-      const elements = gigs.map(gigToTemplateElement);
+      const elements = gigs.map(gigToElement);
 
       return {
         attachment: {
